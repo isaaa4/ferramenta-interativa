07225007ed6dd80f41c6bcf778feea2efd811102
isaaa4 <161043978+isaaa4@users.noreply.github.com>
2024-11-01 10:43:47 -0300

    Add files via upload

---
+++ isa/flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é index.html?',
+    'É o arquivo principal de um site.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é VSCode?',
+    'É um programa para escrever e editar código.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'O que é o JavaScript?',
+    'Uma linguagem que torna páginas web interativas.'
+)
